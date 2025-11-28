3ce21f55e1007c9ed6a5151740b497cae1dc6f3c
Mutahhar Aquib <mohammadmutahharaquib@gmail.com>
2025-11-28 08:33:44 +0530

    minor fix

--- server.js
+++ server.js
@@ -118,4 +118,6 @@ app.post("/delete/:id", requireLogin, async (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running...");
+});
